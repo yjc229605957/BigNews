90caec2440a7e0fad11c745f546d0609f420bddb
yjc229605957 <1198472052@qq.com>
2019-11-17 22:21:33 +0800

    bug多多

--- BigNews/admin/js/article_list.js
+++ BigNews/admin/js/article_list.js
@@ -14,35 +14,18 @@ $(function () {
     //二 .筛选按钮获取数据
     $('#btnSearch').on('click', function (e) {
         e.preventDefault();
-        console.log(currentPage);
-        $.ajax({
-            url: BigNew.CGquery,
-            dataType: 'json',
-            data: {
-                page:currentPage(),
-                perpage: 13,
-                type: $('#selCategory').val(),
-                state: $('#selStatus').val()
-            },
-            success: function (Data) {
-                console.log(Data);
-                var newHtml = template('wenzhang_list', Data.data)
-                $('.table-striped>tbody').html(newHtml)
-            }
-        })
+        getArticleList()
     })
     //进入页面获取所有文章数据
     $('#btnSearch').trigger('click')
 
-    //分页按钮点击切换文章
-    $('#pager').on('click','a',function(){
-        console.log('s');
-        
+    //根据页数请求文章列表
+    function getArticleList(currentPage) {
         $.ajax({
             url: BigNew.CGquery,
             dataType: 'json',
             data: {
-                // page:currentPage(),
+                page: currentPage,
                 perpage: 13,
                 type: $('#selCategory').val(),
                 state: $('#selStatus').val()
@@ -51,7 +34,25 @@ $(function () {
                 console.log(Data);
                 var newHtml = template('wenzhang_list', Data.data)
                 $('.table-striped>tbody').html(newHtml)
+                fenye(Data.data.totalCount,Data.data.totalPage)
+                
+                currentPage()
             }
         })
-    })
+    }
+    //封装分页代码
+    function fenye(totalPages, startPage) {
+        $("#pager").zPager({
+            totalData: totalPages,
+            htmlBox: $('#wraper'),
+            pageCount: startPage,
+            btnShow: true,
+            ajaxSetData: false,
+            dataRender: function (data) {
+                console.log(data + '---data-2');
+            },
+        })
+    };
+    
+
 })
--- BigNews/admin/分页插件/jquery.z-pager.js
+++ BigNews/admin/分页插件/jquery.z-pager.js
@@ -1,68 +1,68 @@
-; (function ($) {
+;(function($){
 	var methods = {
-		pageInit: function (options) {
+		pageInit: function(options){
 			/**
 			 * [opts this plug propertys]
 			 * @type {Obeject}
 			 */
-			var opts = $.extend({}, $.fn.zPager.defaults, options);
-			return $(this).each(function (k, v) {
+			var opts = $.extend({},$.fn.zPager.defaults,options);
+			return $(this).each(function(k,v){
 				var _v = $(v);
-				_v.data("options", opts);
+				_v.data("options",opts);
 				methods.pageData(_v, opts.current);
 			})
 		},
-		pageData: function (_v, _current) {
+		pageData: function(_v, _current){
 			var opts = _v.data("options");
 			var t = opts.totalData, p = opts.pageData, ajaxOpts = null;
-			if (opts.ajaxSetData && (typeof (opts.ajaxSetData) === 'boolean')) {
-				if (opts.url !== '' && typeof (opts.url) === 'string') {
+			if(opts.ajaxSetData&&(typeof(opts.ajaxSetData)==='boolean')){
+				if(opts.url!=='' && typeof(opts.url)==='string'){
 					ajaxOpts = methods.ajaxData(opts.url, _current);
 					t = opts.totalData = ajaxOpts.total;
-					if (ajaxOpts.rows.length > 0) {
-						var ishasDataRender = (opts.dataRender && typeof (opts.dataRender) === 'function');
-						ishasDataRender ? opts.dataRender(ajaxOpts.rows) : methods.dataRender(_v, ajaxOpts.rows);
+					if(ajaxOpts.rows.length>0){
+						var ishasDataRender = (opts.dataRender && typeof(opts.dataRender)==='function');
+							ishasDataRender ? opts.dataRender(ajaxOpts.rows) : methods.dataRender(_v, ajaxOpts.rows);
 					}
-				} else {
+				}else{
 					$.pageError(2);
 				}
 			}
-			if (t % p === 0) {
-				opts.pageCount = parseInt(t / p);
-			} else {
-				opts.pageCount = parseInt(t / p) + 1;
-			}
-			if (opts.pageCount > 0) {
-				_v.data("options", opts);
-				methods.pageRender(_v, _current);
-			}
+			if(t%p === 0){
+                opts.pageCount = parseInt(t/p);
+            }else{
+                opts.pageCount = parseInt(t/p)+1;
+            }
+            if(opts.pageCount>0){
+            	_v.data("options",opts);
+            	methods.pageRender(_v, _current);
+            }
 		},
-		dataRender: function (_v, _data) {
-
+		dataRender: function(_v, _data){
+			
 			var opts = _v.data("options");
 			var cells = '';
-			for (var i = 0; i < _data.length; i++) {
-				cells += '<div class="cc_cells"><a href=""><span>' + _data[i].id + '-' + Math.random() + '</span>';
-				cells += '<span>' + _data[i].title + '</span>';
-				cells += '<span>' + _data[i].starttime + '</span>';
-				cells += '<span>' + _data[i].endtime + '</span>';
-				cells += '</a></div>';
-			}
-			if (opts.htmlBox === '' || (typeof (opts.htmlBox) !== 'Obeject')) {
+				for(var i=0;i<_data.length;i++){
+					cells += '<div class="cc_cells"><a href=""><span>'+_data[i].id+'-'+Math.random()+'</span>';
+					cells += '<span>'+_data[i].title+'</span>';
+					cells += '<span>'+_data[i].starttime+'</span>';
+					cells += '<span>'+_data[i].endtime+'</span>';
+					cells += '</a></div>';
+				}
+			if(opts.htmlBox===''||(typeof(opts.htmlBox)!=='Obeject')){
 				var abx = _v.prev();
-				if (!abx.hasClass('pagerHtmlWrap')) {
+				if(!abx.hasClass('pagerHtmlWrap')){
 					var d = '<div class="pagerHtmlWrap"></div>';
 					_v.before(d);
 				}
 				_v.prev().html(cells);
-			} else {
+			}else{
 				opts.htmlBox.html(cells);
 			}
 		},
-		pageRender: function (_v, _current) {
+		pageRender: function(_v, _current){
 			// 椤电爜鎿嶄綔
 			currentPage(_current);
-
+			
 			/**
 			 * [o this plug propertys]
 			 * @type {Obeject}
@@ -70,158 +70,158 @@
 			var o = _v.data("options");
 			var _page = o.pageCount;
 
-			var _middle = parseInt(o.pageStep / 2);
-			var _tep = _middle - 2;
+			var _middle = parseInt(o.pageStep/2);
+			var _tep = _middle-2;
 			var _html = '';
-			if (_page > o.pageStep && _current <= _page) {
+			if(_page>o.pageStep&&_current<=_page){
 				_html += methods.setPrevNext(o, 'prev');
-				if (_current <= _middle) {
+				if(_current<=_middle){
 					_html += methods.forEach(1, o.pageStep, _current, o.active);
 					_html += methods.elliPsis();
-				} else if (_current > _middle && _current < (_page - _tep)) {
+				}else if(_current>_middle&&_current<(_page-_tep)){
 					_html += methods.pageBtn(1);
 					_html += methods.elliPsis();
-					_html += methods.forEach(_current - _tep, _current - (-_tep) - (-1), _current, o.active);
+					_html += methods.forEach(_current-_tep, _current-(-_tep)-(-1), _current, o.active);
 					_html += methods.elliPsis();
-				} else if (_current >= (_page - _tep)) {
+				}else if(_current>=(_page-_tep)){
 					_html += methods.pageBtn(1);
 					_html += methods.elliPsis();
-					_html += methods.forEach(_page - 2 * _tep - 1, _page - (-1), _current, o.active);
+					_html += methods.forEach(_page-2*_tep-1, _page-(-1), _current, o.active);
 				}
 				_html += methods.setPrevNext(o, 'next');
-			} else if (_page <= o.pageStep) {
-				if (_page > o.minPage) {
+			}else if(_page<=o.pageStep){
+				if(_page>o.minPage){
 					_html += methods.setPrevNext(o, 'prev');
 				}
-				_html += methods.forEach(1, _page - (-1), _current, o.active);
-				if (_page > o.minPage) {
+				_html += methods.forEach(1, _page-(-1), _current, o.active);
+				if(_page>o.minPage){
 					_html += methods.setPrevNext(o, 'next');
-				}
+				}	
 			}
 			_v.html(_html);
 			methods.bindEvent(_v);
 		},
-		bindEvent: function (_v) {
+		bindEvent: function(_v){
 			/**
 			 * [o this plug propertys]
 			 * @type {Obeject}
 			 */
 			var o = _v.data("options");
 			var _a = _v.find("a");
-			$.each(_a, function (index, item) {
-				var _this = $(this);
-				_this.on("click", function () {
-					if (_this.attr("disabled")) {
-						return false;
-					}
-					var _p = _this.attr("page-id");
-					o.current = _p;
-					_v.data("options", o);
-					// methods.options.current = _p;
-					methods.pageData(_v, _p);
+				$.each(_a,function(index,item){
+					var _this = $(this);
+					_this.on("click",function(){
+						if(_this.attr("disabled")){
+							return false;
+						}
+						var _p = _this.attr("page-id");
+						o.current = _p;
+						_v.data("options",o);
+						// methods.options.current = _p;
+						methods.pageData(_v, _p);
+					})
 				})
-			})
 		},
-		forEach: function (_start, length, _current, curclass) {
+		forEach: function(_start,length,_current,curclass){
 			/**
 			 * [s page elements]
 			 * @type {String}
 			 */
 			var s = '';
-			for (var i = _start; i < length; i++) {
-				if (i === parseInt(_current)) {
-					s += methods.pageCurrent(i, curclass);
-				} else {
+			for(var i = _start;i<length;i++){
+				if(i === parseInt(_current)){
+					s += methods.pageCurrent(i,curclass);
+				}else{
 					s += methods.pageBtn(i);
 				}
 			}
 			return s;
 		},
-		pageCurrent: function (_id, _class) {
+		pageCurrent: function(_id,_class){
 			/**
 			 * [class current page element calss]
 			 * @type {String}
 			 */
-			return '<span class="' + _class + '" page-id="' + _id + '">' + _id + '</span>';
+			return '<span class="'+_class+'" page-id="'+_id+'">'+_id+'</span>';
 		},
-		elliPsis: function () {
+		elliPsis: function(){
 			/**
 			 * [class ellipses...]
 			 * @type {String}
 			 */
 			return '<span class="els">...</span>';
 		},
-		pageBtn: function (_id) {
+		pageBtn: function(_id){
 			/**
 			 * [id page id]
 			 * @type {String}
 			 */
-			return '<a page-id="' + _id + '">' + _id + '</a>';
+			return '<a page-id="'+_id+'">'+_id+'</a>';
 		},
-		addBtn: function (_property, _page, _count) {
+		addBtn: function(_property, _page, _count){
 			/**
 			 * [disabled is it can click button]
 			 * @type {Boolean}
 			 */
 			var disabled = '';
-			if (_count) {
-				disabled = (_page === 0 || _page === _count - (-1)) ? 'disabled="true"' : '';
+			if(_count){
+				disabled = (_page === 0 || _page === _count-(-1)) ? 'disabled="true"':'';
 			}
-			return '<a class="' + _property + '" page-id="' + _page + '" ' + disabled + '></a>';
+			return '<a class="'+_property+'" page-id="'+_page+'" '+disabled+'></a>';
 		},
-		setPrevNext: function (_o, _type) {
+		setPrevNext: function(_o, _type){
 			/**
 			 * [s string create prev or next buttons elements]
 			 * @type {String}
 			 */
 			var s = '';
-			function prev() {
-				if (_o.btnShow) {
-					s += methods.addBtn(_o.firstBtn, 1);
+			function prev(){
+				if(_o.btnShow){
+					s += methods.addBtn(_o.firstBtn, 1); 
 				}
-				if (_o.btnBool) {
-					s += methods.addBtn(_o.prevBtn, _o.current - 1, _o.pageCount);
+				if(_o.btnBool){
+					s += methods.addBtn(_o.prevBtn, _o.current-1, _o.pageCount);
 				}
 				return s;
 			}
-			function next() {
-				if (_o.btnBool) {
-					s += methods.addBtn(_o.nextBtn, _o.current - (-1), _o.pageCount);
+			function next(){
+				if(_o.btnBool){
+					s += methods.addBtn(_o.nextBtn, _o.current-(-1), _o.pageCount);
 				}
-				if (_o.btnShow) {
+				if(_o.btnShow){
 					s += methods.addBtn(_o.lastBtn, _o.pageCount);
 				}
 				return s;
 			}
-			return _type === 'prev' ? prev() : next();
+			return _type==='prev'? prev(): next();
 		},
-		ajaxData: function (_url, _current) {
+		ajaxData: function(_url, _current){
 			/**
 			 * [ajax get data and pagenumber]
 			 * @param  {Object} ){ var parms [ajax url,current page number]
 			 * @return {[type]}            [obj total rows]
 			 */
 			var _total = $.fn.zPager.defaults.totalData;
-			return (function () {
-				var parms = { 'total': _total, 'rows': [] };
+			return (function(){
+				var parms = {'total':_total,'rows':[]};
 				$.ajax({
 					url: _url,
 					type: 'get',
-					data: { "page": _current },
-					dataType: 'json',
-					cache: false,
-					async: false,
-					success: function (data) {
-						if (data.total && (data.total !== 0)) {
-							parms['total'] = data.total;
-							parms['rows'] = data.rows;
-						} else {
-							$.pageError(3);
-						}
-					},
-					error: function (XMLHttpRequest, textStatus, errorThrown) {
+					data: {"page":_current},
+	                dataType: 'json',
+	                cache : false,  
+        			async : false,
+	                success: function(data) {
+	                	if(data.total && (data.total!==0)){
+	                        parms['total'] = data.total;
+	                        parms['rows'] = data.rows;
+	                	}else{
+	                		$.pageError(3);
+	                	}
+	                },
+					error: function(XMLHttpRequest,textStatus,errorThrown) {
 						var msg = '';
-						switch (XMLHttpRequest.readyState) {
+						switch(XMLHttpRequest.readyState){
 							case 0:
 								msg = '锛堟湭鍒濆鍖栵級杩樻病鏈夎皟鐢╯end()鏂规硶';
 								break;
@@ -238,7 +238,7 @@
 								msg = '锛堝畬鎴愶級鍝嶅簲鍐呭瑙ｆ瀽瀹屾垚锛屽彲浠ュ湪瀹㈡埛绔皟鐢ㄤ簡';
 								break;
 						}
-						console.log(textStatus + '锛�' + XMLHttpRequest.readyState + '-' + msg);
+						console.log(textStatus+'锛�'+XMLHttpRequest.readyState+'-'+msg);
 					}
 				})
 				return parms;
@@ -247,15 +247,15 @@
 	}
 
 	$.extend({
-		pageError: function (type) {
+		pageError:function(type){
 			/**
 			 * [switch error type]
 			 * @param  {[type]} type [no this function]
 			 * @return {[type]}      [ajax error]
 			 */
-			switch (type) {
+			switch(type){
 				case 1:
-					console.log('method' + method + 'dose not exist on jQuery.zPager');
+					console.log('method'+method+'dose not exist on jQuery.zPager');
 					break;
 				case 2:
 					console.log('no ajax');
@@ -270,42 +270,40 @@
 	})
 
 	$.fn.extend({
-		zPager: function (method) {
+		zPager:function(method){
 			/**
 			 * [if has this method]
 			 * @param  {[type]} methods[method] [apply this method]
 			 * @return {[type]}                 [return property]
 			 */
-			if (methods[method]) {
+			if(methods[method]){
 				return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-			} else if (typeof method === 'object' || !method) {
+			}else if(typeof method === 'object' || !method){
 				return methods.pageInit.apply(this, arguments);
-			} else {
+			}else{
 				$.pageError(1);
 			}
 		}
 	})
 
 	$.fn.zPager.defaults = {
-		totalData: 10, //数据总条数
-		pageData: 5, //每页数据条数
-		pageCount: 0, //总页数
-		current: 1, //当前页码数
-		pageStep: 8, //当前可见最多页码个数
-		minPage: 5, //最小页码数，页码小于此数值则不显示上下分页按钮
-		active: 'current', //当前页码样式
-		prevBtn: 'pg-prev', //上一页按钮
-		nextBtn: 'pg-next', //下一页按钮
-		btnBool: true, //是否显示上一页下一页
-		firstBtn: 'pg-first', //第一页按钮
-		lastBtn: 'pg-last', //最后一页按钮
-		btnShow: true, //是否显示第一页和最后一页按钮
-		disabled: true, //按钮失效样式
-		ajaxSetData: true, //是否使用ajax获取数据 此属性为真时需要url和htmlBox不为空
-		url: '', //ajax路由
-		htmlBox: '' //ajax数据写入容器
+		totalData: 10, //鏁版嵁鎬绘潯鏁�
+		pageData: 5, //姣忛〉鏁版嵁鏉℃暟
+		pageCount: 0, //鎬婚〉鏁�
+		current: 1, //褰撳墠椤电爜鏁�
+		pageStep: 8, //褰撳墠鍙鏈€澶氶〉鐮佷釜鏁�
+		minPage: 5, //鏈€灏忛〉鐮佹暟锛岄〉鐮佸皬浜庢鏁板€煎垯涓嶆樉绀轰笂涓嬪垎椤垫寜閽�
+		active: 'current', //褰撳墠椤电爜鏍峰紡
+		prevBtn: 'pg-prev', //涓婁竴椤垫寜閽�
+		nextBtn: 'pg-next', //涓嬩竴椤垫寜閽�
+		btnBool: true, //鏄惁鏄剧ず涓婁竴椤典笅涓€椤�
+		firstBtn: 'pg-first', //绗竴椤垫寜閽�
+		lastBtn: 'pg-last', //鏈€鍚庝竴椤垫寜閽�
+		btnShow: true, //鏄惁鏄剧ず绗竴椤靛拰鏈€鍚庝竴椤垫寜閽�
+		disabled: true, //鎸夐挳澶辨晥鏍峰紡
+		ajaxSetData: true, //鏄惁浣跨敤ajax鑾峰彇鏁版嵁 姝ゅ睘鎬т负鐪熸椂闇€瑕乽rl鍜宧tmlBox涓嶄负绌�
+		url: '', //ajax璺敱
+		htmlBox: '' //ajax鏁版嵁鍐欏叆瀹瑰櫒
 	}
-
-
 
 })(jQuery)
